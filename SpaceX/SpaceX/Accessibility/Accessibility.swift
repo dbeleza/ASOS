cfc7a1515461401eeed10e799b32343af4a524e3
David Beleza <david.beleza@gmail.com>
2021-10-23 09:40:47 +0100

    Add accessibility struct for UI Tests

---
+++ SpaceX/SpaceX/Accessibility/Accessibility.swift
@@ -0,0 +1,18 @@
+//
+//  Accessibility.swift
+//  SpaceX
+//
+//  Created by David Beleza on 23/10/2021.
+//
+
+import Foundation
+
+struct Accessibility {
+    struct Screen {
+        struct Launch {
+            static let filterButton = "launch_screen_filter_button"
+            static let tableView = "launch_screen_table_view"
+            static let spinner = "launch_screen_spinner"
+        }
+    }
+}
