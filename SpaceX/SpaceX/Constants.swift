5c142a218591b1246d331fbcb1508eb8964ec2df
David Beleza <david.beleza@gmail.com>
2021-10-23 09:41:01 +0100

    Update Constants file

--- SpaceX/SpaceX/Constants.swift
+++ SpaceX/SpaceX/Constants.swift
@@ -9,18 +9,7 @@ import Foundation
 import UIKit
 
 struct Constants {
-    struct API {
-        static let scheme = "https"
-        static var host: String {
-            guard let host = Bundle.main.infoDictionary?["Host"] as? String else { fatalError("Expected to have a host defined 💩") }
-            return host
-        }
-        static let postHttpMethod = "POST"
-        static let jsonContentType = "application/json"
-        static let contentTypeHeader = "Content-Type"
-    }
-
-    struct Margin {
+    struct UI {
         static let baseline: CGFloat = 8.0
     }
 
